d7477378359055429e54c12eb9a4ac7cc7dcb389
bkdhanush <bkdhanushofficial@gmail.com>
2023-12-10 16:21:13 +0530

    add: Custom colors in tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        'light-black': '#171717',
+        'primary-color': '#FD853A'
+      }
     },
   },
   plugins: [],
