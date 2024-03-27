abbfc44c78a99fddc5c30a104973a875d17beae2
bkdhanush <bkdhanushofficial@gmail.com>
2024-03-27 10:26:52 +0530

    clean: unwanted projects components

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import GetAdminInput from '@/components/GetAdminInput'
-import AdminLogin from '@/components/AdminLogin';
+
 export const metadata ={
   title: 'Projects'
 }
 
 function page() {
   return (
-    <div><AdminLogin /></div>
+    <div>Projects</div>
   )
 }
 
